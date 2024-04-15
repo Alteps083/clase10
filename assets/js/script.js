6db9849d2d9dca3a39308239aaa16ade705b6c8a
Alexis Peñailillo <alexis.ptello@gmail.com>
2024-04-15 15:58:04 -0400

    class10 v1.0.0

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+
+/*var nombre = "hola"
+nombre = 2;
+console.log("hola");*/
+let nombreV = "";
+let apellidoV = "";
+let edadV = 0;
+
+
+
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let edad = document.getElementById("edad");
+
+let btnenviar = document.getElementById("btnenviar");
+
+btnenviar.addEventListener("click", (e)=>{
+    e.preventDefault();
+    nombreV = nombre.value;
+    apellidoV = apellido.value;
+    edadV = edad.value;
+
+    console.log(`nombre : ${nombreV} y el apellido es: ${apellidoV} y la edad es: ${edadV}`);
+});
